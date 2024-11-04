2b699ae523014f1739730abbd6bf0c9921da50d3
rchesaux <chesaux66@gmail.com>
2024-11-04 12:28:03 -0500

    2nd commit

--- index.js
+++ index.js
@@ -7,12 +7,23 @@ const updateList= () => {
     let userItem = document.getElementById('user-input').value;
     let newText = document.createTextNode(userItem);
 
+    let deleteButton = document.createElement('button')
+    deleteButton.innerHTML = '<button onclick="deleteItem">'+ 'Delete' + '<button>';
+
+    
+
     newItem.appendChild(newText);
 
     let position = document.getElementsByTagName('ul')[0];
 
+    // add to list with delete button
     position.appendChild(newItem);
+    newItem.appendChild(deleteButton)
 
+    // clear text box
     document.getElementById("user-input").value = "";
 
+    
+
 }
+
